758b60f4689d5c112cdbe31b7757f70cc7b7c391
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2024-01-10 13:39:42 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+// Adding Entity Classes
+public class Product
+{
+    // Primary Key
+    public int Id { get; set; }
+    [MaxLength(50)]
+    [Required]
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Category
+{
+    // Primary Key
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Merhaba, dünya!");
+    }
+}
